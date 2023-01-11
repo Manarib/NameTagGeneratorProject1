a6b7e129c3c0876a9c5431d24c5a8cd9be3f1f36
batool <batoola2027@gmail.com>
2023-01-11 16:01:17 +0200

    update

---
+++ index.js
@@ -0,0 +1,27 @@
+var button = document.getElementsByTagName("button");
+button[0].addEventListener(
+  "click",
+  function addTag(e) {
+    e.preventDefault();
+
+    let itemtag = document.createElement("div");
+    itemtag.classList.add("tags");
+
+    let valueinput = document.querySelector("input");
+
+    console.log(valueinput.value);
+
+    console.log(itemtag);
+    itemtag.innerHTML = `
+    <div><i class="fa-solid fa-trash"></i> </div>
+       <h1>Hello</h1>
+      <h5>my name is</h5>
+      <p> ${valueinput.value} </p>
+    `;
+    valueinput.value = "";
+
+    let tags = document.getElementsByClassName("tags-name");
+    tags[0].appendChild(itemtag);
+  },
+  false
+);
